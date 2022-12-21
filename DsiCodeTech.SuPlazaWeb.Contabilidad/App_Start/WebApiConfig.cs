faf11ee80a2e7a062fbe20ec55e4862790ac375a
Erik Guerrero Bravo <eguerrero@uttt.edu.mx>
2022-12-21 16:30:29 -0600

    se agregan clases y se agran las librerias que habilitan cors para la implementación del proyecto.

--- DsiCodeTech.SuPlazaWeb.Contabilidad/App_Start/WebApiConfig.cs
+++ DsiCodeTech.SuPlazaWeb.Contabilidad/App_Start/WebApiConfig.cs
@@ -1,7 +1,11 @@
-﻿using System;
+﻿using System.Web.Http;
+using System.Web.Http.Cors;
+using System.Web.Http.ExceptionHandling;
+using System.Net.Http.Headers;
+using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Web.Http;
+using DsiCodeTech.SuPlazaWeb.Contabilidad.Handler.ExceptionHandler;
 
 namespace DsiCodeTech.SuPlazaWeb.Contabilidad
 {
@@ -10,7 +14,10 @@ namespace DsiCodeTech.SuPlazaWeb.Contabilidad
         public static void Register(HttpConfiguration config)
         {
             // Configuración y servicios de Web API
-
+            //habilitamos cors para nuestra web api
+            //primer elemento debe ser el hosting donde se publica el proyecto: www.example.com
+            var cors = new EnableCorsAttribute("*", "*", "*");
+            config.EnableCors(cors);
             // Rutas de Web API
             config.MapHttpAttributeRoutes();
 
@@ -19,6 +26,14 @@ namespace DsiCodeTech.SuPlazaWeb.Contabilidad
                 routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
+            config.Filters.Add(new SuPlazaExceptionFilterAttribute());
+            config.Services.Replace(typeof(IExceptionHandler), new SuPlazaExceptionHandler());
+
+            /* Eliminar formatos XML para las respuestas */
+            config.Formatters.XmlFormatter.SupportedMediaTypes.Clear();
+
+            /* Json por defecto en las respuestas */
+            config.Formatters.JsonFormatter.SupportedMediaTypes.Add(new MediaTypeHeaderValue("application/json"));
         }
     }
 }
---
+++ DsiCodeTech.SuPlazaWeb.Contabilidad/Handler/ExceptionHandler/SuPlazaExceptionHandler.cs
@@ -0,0 +1,42 @@
+﻿using DsiCodeTech.SuPlazaWeb.Contabilidad.Dto;
+using Microsoft.Extensions.Logging;
+using NLog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Formatting;
+using System.Web;
+using System.Web.Http.ExceptionHandling;
+using System.Web.Http.Results;
+
+namespace DsiCodeTech.SuPlazaWeb.Contabilidad.Handler.ExceptionHandler
+{
+    public class SuPlazaExceptionHandler : System.Web.Http.ExceptionHandling.ExceptionHandler
+    {
+        private static readonly Logger Log = LogManager.GetCurrentClassLogger();
+
+        public override void Handle(ExceptionHandlerContext context)
+        {
+            Exception exception = context.Exception;
+
+            if (context.ExceptionContext.ActionContext != null && context.ExceptionContext.ControllerContext != null)
+            {
+                Log.Error(exception,
+                    $"Error generado en {context.ExceptionContext.ActionContext.ActionDescriptor.ActionName} " +
+                    $"en el controller {context.ExceptionContext.ActionContext.ControllerContext.ControllerDescriptor.ControllerName}");
+
+            }
+
+            var response = context.Request
+                .CreateResponse(HttpStatusCode.Conflict,
+                new HttpResponseOnError { Description = context.Exception.Message },
+                new JsonMediaTypeFormatter() { SerializerSettings = { ContractResolver = new JsonLowerCaseResolver() } });
+
+            context.Result = new ResponseMessageResult(response);
+            base.Handle(context);
+        }
+
+    }
+}
