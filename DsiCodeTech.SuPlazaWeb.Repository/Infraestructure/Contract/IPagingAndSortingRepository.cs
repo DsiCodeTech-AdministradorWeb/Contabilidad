c738223fa3a96a8392349afe7fbc90927015fdf6
Erik Guerrero Bravo <eguerrero@uttt.edu.mx>
2022-12-21 16:59:18 -0600

    se agrega interfaz de paginacion

---
+++ DsiCodeTech.SuPlazaWeb.Repository/Infraestructure/Contract/IPagingAndSortingRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DsiCodeTech.SuPlazaWeb.Repository.Infraestructure.Contract
+{
+    public interface IPagingAndSortingRepository<T>
+    {
+        IEnumerable<T> GetPaging(Expression<Func<T, string>> orderBy, int page_number, int page_size);
+
+        IEnumerable<T> GetPaging(Expression<Func<T, bool>> where, Expression<Func<T, string>> orderBy, int page_number, int page_size);
+
+        IEnumerable<T> GetPagingDescending(Expression<Func<T, string>> orderBy, int page_number, int page_size);
+
+        IEnumerable<T> GetPagingDescending(Expression<Func<T, bool>> where, Expression<Func<T, string>> orderBy, int page_number, int page_size);
+
+    }
+}
