9b13fafc956302b8a5fda002d6874a07ced0a40a
Erik Guerrero Bravo <eguerrero@uttt.edu.mx>
2022-12-21 17:09:58 -0600

    se agrega clase de unidad de trabajo

---
+++ DsiCodeTech.SuPlazaWeb.Repository/Infraestructure/UnitOfWork.cs
@@ -0,0 +1,29 @@
+﻿using DsiCodeTech.SuPlazaWeb.Repository.Infraestructure.Contract;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DsiCodeTech.SuPlazaWeb.Repository.Infraestructure
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        //private readonly Dc_PosAdminEntities _dbContext;
+
+        public UnitOfWork()
+        {
+           // _dbContext = new Dc_PosAdminEntities();
+        }
+
+        public DbContext Db
+        {
+            get { return _dbContext; }
+        }
+
+        public void Dispose()
+        {
+        }
+    }
+}
