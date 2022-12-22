fbc6a7906ea906fef36e92bf7aaed338711f8f24
Erik Guerrero Bravo <eguerrero@uttt.edu.mx>
2022-12-21 20:12:37 -0600

    se agrega clase de excepcion

---
+++ DsiCodeTech.SuPlazaWeb.Domain/Exception/BusinessException.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DsiCodeTech.SuPlazaWeb.Domain.Exception
+{
+
+    [Serializable]
+    public class BusinessException : System.Exception
+    {
+        public BusinessException(string message) : base(message)
+        {
+
+        }
+
+        public BusinessException(string message, System.Exception exception) : base(message, exception)
+        {
+
+        }
+
+        protected BusinessException(SerializationInfo info, StreamingContext context) : base(info, context) { }
+    }
+}
