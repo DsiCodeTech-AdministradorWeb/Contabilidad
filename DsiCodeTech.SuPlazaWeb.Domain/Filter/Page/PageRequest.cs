bf783977b91526ec6c5c0da79f65844f951ea705
Erik Guerrero Bravo <eguerrero@uttt.edu.mx>
2022-12-21 20:36:51 -0600

    se agrega clases de paginacion validas para  peticiones http

---
+++ DsiCodeTech.SuPlazaWeb.Domain/Filter/Page/PageRequest.cs
@@ -0,0 +1,31 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DsiCodeTech.SuPlazaWeb.Domain.Filter.Page
+{
+    public class PageRequest
+    {
+        [JsonProperty(PropertyName = "page_number")]
+        public int pageNumber { get; set; }
+
+        [JsonProperty(PropertyName = "page_size")]
+        public int pageSize { get; set; }
+
+        [JsonProperty(PropertyName = "sort")]
+        public Sorting sort { get; set; }
+
+        public PageRequest() { }
+
+
+        public PageRequest(int PageSize, int PageNumber, string Sort)
+        {
+            this.pageSize = PageSize;
+            this.pageNumber = PageNumber;
+            this.sort = new Sorting(Sort.Split(',')[0], Sort.Split(',')[1] == "asc" ? Direction.Ascending : Direction.Descending);
+        }
+    }
+}
---
+++ DsiCodeTech.SuPlazaWeb.Domain/Filter/Page/PageResponse.cs
@@ -0,0 +1,38 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DsiCodeTech.SuPlazaWeb.Domain.Filter.Page
+{
+    public class PageResponse<T> where T : class
+    {
+        [JsonProperty(propertyName: "content")]
+        public List<T> Content { get; set; }
+
+        [JsonProperty("total_elements")]
+        public int TotalElements { get; set; }
+
+        [JsonProperty(propertyName: "total_pages")]
+        public int TotalPages { get; set; }
+
+        [JsonProperty(propertyName: "size")]
+        public int Size { get; set; }
+
+        [JsonProperty(propertyName: "number")]
+        public int Number { get; set; }
+
+        public PageResponse(List<T> items, int count, int pageNumber, int pageSize)
+        {
+            TotalElements = count;
+            TotalPages = (int)Math.Ceiling(count / (double)pageSize);
+            Size = pageSize;
+            Number = pageNumber;
+            Content = items;
+        }
+
+        public PageResponse() { }
+    }
+}
