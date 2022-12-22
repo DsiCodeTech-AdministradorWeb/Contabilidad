512b8ddc35e9867cd5252f8b1c17ede170a33b9b
Erik Guerrero Bravo <eguerrero@uttt.edu.mx>
2022-12-21 20:58:14 -0600

    se genero la clase articuloBusiness con la implementacion de la interface

---
+++ DsiCodetech.SuPlazaWeb.Business/ArticuloBusiness.cs
@@ -0,0 +1,22 @@
+﻿using DsiCodetech.SuPlazaWeb.Business.Interface;
+using DsiCodeTech.SuPlazaWeb.Repository;
+using DsiCodeTech.SuPlazaWeb.Repository.Infraestructure.Contract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DsiCodetech.SuPlazaWeb.Business
+{
+    public class ArticuloBusiness:IArticuloBusiness
+    {
+        private readonly IUnitOfWork unitOfWork;
+        private readonly ArticuloRepository repository;
+        public ArticuloBusiness(IUnitOfWork _unitOfWork)
+        {
+            unitOfWork = _unitOfWork;
+            repository = new ArticuloRepository(unitOfWork);
+        }
+    }
+}
