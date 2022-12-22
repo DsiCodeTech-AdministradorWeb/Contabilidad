b785fd8ac9232976795b7fcce42ad6c37fe49809
Erik Guerrero Bravo <eguerrero@uttt.edu.mx>
2022-12-21 20:38:49 -0600

    se agrega la clase Sorting

---
+++ DsiCodeTech.SuPlazaWeb.Domain/Filter/Sort/Sorting.cs
@@ -0,0 +1,26 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DsiCodeTech.SuPlazaWeb.Domain.Filter.Sort
+{
+    public class Sorting
+    {
+        [JsonProperty(PropertyName = "name")]
+        public string Name { get; set; }
+
+        [JsonProperty(PropertyName = "direction")]
+        public Direction Direction { get; set; }
+
+        public Sorting() { }
+
+        public Sorting(string Name, Direction Direction)
+        {
+            this.Name = Name;
+            this.Direction = Direction;
+        }
+    }
+}
