b4d03733a0455806a1f885031a04f81e72d91e2f
Erik Guerrero Bravo <eguerrero@uttt.edu.mx>
2022-12-25 15:41:42 -0600

    se agrega libreria de linqkit ademas de las modificaciones correspondientes a la implementacion de la interfaz

--- DsiCodeTech.SuPlazaWeb.Repository/ArticuloRepository.cs
+++ DsiCodeTech.SuPlazaWeb.Repository/ArticuloRepository.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
+using LinqKit;
+using LinqKit.Core;
 
 namespace DsiCodeTech.SuPlazaWeb.Repository
 {
