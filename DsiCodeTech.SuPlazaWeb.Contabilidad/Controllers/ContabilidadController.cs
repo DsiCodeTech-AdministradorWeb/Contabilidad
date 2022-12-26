e747aa8509d70dec82a46ff84d79e28da0e8d04a
Erik Guerrero Bravo <eguerrero@uttt.edu.mx>
2022-12-26 16:19:06 -0600

    se modifica interfaz grafica

---
+++ DsiCodeTech.SuPlazaWeb.Contabilidad/Controllers/ContabilidadController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DsiCodeTech.SuPlazaWeb.Contabilidad.Controllers
+{
+    public class ContabilidadController : Controller
+    {
+        // GET: Contabilidad
+        public ActionResult mostrar()
+        {
+            return View();
+        }
+    }
+}
