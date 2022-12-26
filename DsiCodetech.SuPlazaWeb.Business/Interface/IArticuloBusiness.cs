de9b6dee3413250a338d47974e0cdc675ac887b9
Erik Guerrero Bravo <eguerrero@uttt.edu.mx>
2022-12-25 21:15:11 -0600

    se agregan los metodos para la implementacion de  consulta

--- DsiCodetech.SuPlazaWeb.Business/Interface/IArticuloBusiness.cs
+++ DsiCodetech.SuPlazaWeb.Business/Interface/IArticuloBusiness.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using DsiCodeTech.SuPlazaWeb.Domain;
+using DsiCodeTech.SuPlazaWeb.Domain.Filter.Model;
+using DsiCodeTech.SuPlazaWeb.Domain.Filter.Page;
+using DsiCodeTech.SuPlazaWeb.Domain.Filter.Query;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +12,7 @@ namespace DsiCodetech.SuPlazaWeb.Business.Interface
 {
     public interface IArticuloBusiness
     {
+        List<ArticuloDM> GetAllEntidades();
+        PageResponse<ArticuloFilterDM> GetArticulosPaging(ArticuloQuery query);
     }
 }
