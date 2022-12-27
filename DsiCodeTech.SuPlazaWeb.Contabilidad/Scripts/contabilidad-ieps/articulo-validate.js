1d6544f6686232ef73a8e13c6377f64cec35a463
Erik Guerrero Bravo <eguerrero@uttt.edu.mx>
2022-12-26 20:14:57 -0600

    se agrega archivo de validacion js

---
+++ DsiCodeTech.SuPlazaWeb.Contabilidad/Scripts/contabilidad-ieps/articulo-validate.js
@@ -0,0 +1,13 @@
+﻿$(document).ready(function () {
+
+    $('#cod_barras').prop('disabled', true);
+    $('#cod_asociado').prop('disabled', true);
+    $('#cod_interno').prop('disabled', true);
+    $('#descripcion_corta').prop('disabled', true);
+    $('#descripcion').prop('disabled', true);
+    $('#cantidad_um').prop('disabled', true);
+    $('#precio_compra').prop('disabled', true);
+    $('#utilidad').prop('disabled', true);
+
+
+})
