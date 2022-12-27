62bbc73e24e6b2cbaf0d2816514b5fc04130d3d8
Erik Guerrero Bravo <eguerrero@uttt.edu.mx>
2022-12-26 20:44:56 -0600

    se agrega api de inicio sin elelementos de configuración.

---
+++ DsiCodeTech.SuPlazaWeb.Contabilidad/Api/ArticulosController.cs
@@ -0,0 +1,13 @@
+﻿using DsiCodeTech.SuPlazaWeb.Contabilidad.Handler.ExceptionHandler;
+using System.Web.Http;
+using System.Web.Http.Cors;
+
+namespace DsiCodeTech.SuPlazaWeb.Contabilidad.Api
+{
+    [SuPlazaExceptionFilter]
+    [EnableCors(origins: "*", headers: "*", methods: "*")]
+    [RoutePrefix(prefix: "api/articulos")]
+    public class ArticulosController : ApiController
+    {
+    }
+}
