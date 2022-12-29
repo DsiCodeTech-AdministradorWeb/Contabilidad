50759e0a800070aa67aafb580a378d49cba96c15
Erik Guerrero Bravo <eguerrero@uttt.edu.mx>
2022-12-29 17:38:10 -0600

    se agrega la entidad impuestoDto

---
+++ DsiCodeTech.SuPlazaWeb.Contabilidad/Dto/ImpuestoDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DsiCodeTech.SuPlazaWeb.Contabilidad.Dto
+{
+    public class ImpuestoDto
+    {
+        public int id { get; set; }
+        public string cod_barras { get; set; }
+        public double ieps { get; set; }
+        public DateTime fecha_registro { get; set; }
+    }
+}
