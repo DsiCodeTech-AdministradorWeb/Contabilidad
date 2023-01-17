825bbfe90acab86180cc432f4139c7e7e7cc8a77
Erik Guerrero Bravo <eguerrero@uttt.edu.mx>
2023-01-16 22:58:44 -0600

    se  implementa la interfaz usuarioBusiness

--- DsiCodetech.SuPlazaWeb.Business/UsuarioBusiness.cs
+++ DsiCodetech.SuPlazaWeb.Business/UsuarioBusiness.cs
@@ -1,4 +1,5 @@
-﻿using DsiCodeTech.SuPlazaWeb.Domain;
+﻿using DsiCodetech.SuPlazaWeb.Business.Interface;
+using DsiCodeTech.SuPlazaWeb.Domain;
 using DsiCodeTech.SuPlazaWeb.Repository;
 using DsiCodeTech.SuPlazaWeb.Repository.Infraestructure.Contract;
 using System;
@@ -9,7 +10,7 @@ using System.Threading.Tasks;
 
 namespace DsiCodetech.SuPlazaWeb.Business
 {
-    public class UsuarioBusiness
+    public class UsuarioBusiness: IUsuarioBusiness
     {
         /// <summary>
         /// implementacion de la unidad de trabajo
