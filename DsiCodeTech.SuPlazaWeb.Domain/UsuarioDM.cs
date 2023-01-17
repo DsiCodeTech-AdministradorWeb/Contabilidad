98b478412964371c306ad20dc4a55959074589eb
Erik Guerrero Bravo <eguerrero@uttt.edu.mx>
2023-01-16 22:54:10 -0600

    se agregan metodos de consulta de usuarios

--- DsiCodeTech.SuPlazaWeb.Domain/UsuarioDM.cs
+++ DsiCodeTech.SuPlazaWeb.Domain/UsuarioDM.cs
@@ -1,20 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Web;
+using System.Text;
+using System.Threading.Tasks;
 
-namespace DsiCodeTech.SuPlazaWeb.Contabilidad.Dto
+namespace DsiCodeTech.SuPlazaWeb.Domain
 {
-    public class UsuarioDto
+    public class UsuarioDM
     {
         public string User_name { get; set; }
         public string Password { get; set; }
-
-        public string Tipo_usuario{ get; set; }
-
+        public string Tipo_usuario { get; set; }
         public bool Enable { get; set; }
-        public int User_code_bascula { get; set; }
-
+        public short User_code_bascula { get; set; }
         public DateTime Fecha_registro { get; set; }
     }
 }
---
+++ DsiCodetech.SuPlazaWeb.Business/UsuarioBusiness.cs
@@ -0,0 +1,60 @@
+﻿using DsiCodeTech.SuPlazaWeb.Domain;
+using DsiCodeTech.SuPlazaWeb.Repository;
+using DsiCodeTech.SuPlazaWeb.Repository.Infraestructure.Contract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DsiCodetech.SuPlazaWeb.Business
+{
+    public class UsuarioBusiness
+    {
+        /// <summary>
+        /// implementacion de la unidad de trabajo
+        /// </summary>
+        private readonly IUnitOfWork unitOfWork;
+        private readonly UsuarioRepository repository;
+        public UsuarioBusiness(IUnitOfWork _unitOfWork)
+        {
+            unitOfWork = _unitOfWork;
+            repository = new UsuarioRepository(unitOfWork);
+        }
+
+        /// <summary>
+        /// este metodo se encarga de consultar un usaurio por nombre de usuario y password
+        /// </summary>
+        /// <param name="usuarioDM">la entidad del tipo usuario</param>
+        /// <returns>un valor true/false</returns>
+        public bool GetUsuario(UsuarioDM usuarioDM)
+        {
+            bool respuesta = false;
+            var usuario = repository.SingleOrDefault(u => u.user_name.Equals(usuarioDM.User_name) && u.password.Equals(usuarioDM.Password));
+            if (usuario != null)
+            {
+                respuesta = true;
+            }
+            return respuesta;
+        }
+
+        public UsuarioDM ValidarUsuario(UsuarioDM usuarioDM)
+        {
+            var usuario = repository.SingleOrDefault(u => u.user_name.Equals(usuarioDM.User_name) && u.password.Equals(usuarioDM.Password));
+            if (usuario != null)
+            {
+                return new UsuarioDM
+                {
+                    User_name = usuarioDM.User_name,
+                    Password = usuarioDM.Password,  
+                    Enable = usuarioDM.Enable,
+                    Fecha_registro = usuarioDM.Fecha_registro,
+                    Tipo_usuario = usuarioDM.Tipo_usuario,
+                    User_code_bascula= usuarioDM.User_code_bascula
+                };
+            }
+            return null;
+        }
+
+    }
+}
