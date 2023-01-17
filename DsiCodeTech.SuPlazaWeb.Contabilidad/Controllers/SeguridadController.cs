581c7fbf37cdf81cd568f1cfaa5fc8247ceb4309
Erik Guerrero Bravo <eguerrero@uttt.edu.mx>
2023-01-17 00:30:20 -0600

    se agregan dependencias de seguridad para el acceso y los claims de los usuarios

--- DsiCodeTech.SuPlazaWeb.Contabilidad/Controllers/SeguridadController.cs
+++ DsiCodeTech.SuPlazaWeb.Contabilidad/Controllers/SeguridadController.cs
@@ -7,6 +7,9 @@ using System.Linq;
 using System.Web;
 using System.Web.Http.Cors;
 using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
+using Microsoft.Owin.Security;
+using System.Security.Claims;
 
 namespace DsiCodeTech.SuPlazaWeb.Contabilidad.Controllers
 {
@@ -18,13 +21,14 @@ namespace DsiCodeTech.SuPlazaWeb.Contabilidad.Controllers
 
         [HttpGet]
         [AllowAnonymous]
-        public ActionResult Login()
+        public ActionResult Login(string returnUrl)
         {
+            ViewBag.ReturnUrl = returnUrl;
             return View();
         }
 
         [HttpPost]
-        public ActionResult Login([Bind(Include = "User_name,Password")] UsuarioDto usuarioDto) 
+        public ActionResult Login([Bind(Include = "User_name,Password")] UsuarioDto usuarioDto, string returnUrl) 
         {
             if (ModelState.IsValid)
             {
@@ -33,5 +37,32 @@ namespace DsiCodeTech.SuPlazaWeb.Contabilidad.Controllers
             }
             return View();
         }
+
+
+        #region Metodo SigIn
+        private ActionResult SigInUser(UsuarioDto usuarioDto, bool recuerdame, string returnUrl)
+        {
+            ActionResult Result;
+            //un claims puede almacenar cualquier tipo de informacion del usuario, nombre, mail, password, lo que sea
+            List<Claim> Claims = new List<Claim>(); //listado de claims
+            Claims.Add(new Claim(ClaimTypes.NameIdentifier, usuarioDto.User_name)); //primer claims
+            Claims.Add(new Claim(ClaimTypes.Name, usuarioDto.User_name));
+            ///estos claims se almacenan en la cookie para identificar al usuario y sus atributos o permisos
+            var Identity = new ClaimsIdentity(Claims, DefaultAuthenticationTypes.ApplicationCookie); 
+            ///cuando el usuario se logea se crea una cookie de autenticacion 
+           ///este AutenticationManager se encarga de administrarla cookie y da el seguimiento a todo el sitio con todo y los permisos del usuario
+            IAuthenticationManager authenticationManager = HttpContext.GetOwinContext().Authentication;
+            authenticationManager.SignIn(new AuthenticationProperties() { IsPersistent = recuerdame }, Identity); //el rememberMe es para recordarlo true/false
+
+            if (string.IsNullOrWhiteSpace(returnUrl))
+            {
+                returnUrl = Url.Action("mostrar", "Contabilidad");
+            }
+            Result = Redirect(returnUrl);
+            return Result;
+
+        }
+        #endregion
+
     }
 }
